813542db14a2160bea4cd8f0b2663b74e5aaca83
David Maher <sunookitsune@kitsunet.org>
2020-02-06 11:39:36 -0500

    Changed the RGB conversion to use the white LEDs for more than just pure white.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var Service, Characteristic;
 
 const Gpio = require('pigpio').Gpio;
-const converter = require('color-convert');
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
@@ -49,7 +48,7 @@ SmartLedStripAccessory.prototype = {
         informationService
             .setCharacteristic(Characteristic.Manufacturer, 'Sunoo')
             .setCharacteristic(Characteristic.Model, 'RGBW LED Strip')
-            .setCharacteristic(Characteristic.SerialNumber, '6345789');
+            .setCharacteristic(Characteristic.SerialNumber, this.rPin + ':' + this.gPin + ':' + this.bPin + ':' + this.wPin);
 
         let smartLedStripService = new Service.Lightbulb(this.name);
 
@@ -95,31 +94,87 @@ SmartLedStripAccessory.prototype = {
 
     toggleState: function() {
         if (!this.isOn()) {
-            this.updateRGB(0, 0, 0);
+            this.updateRGBW(0, 0, 0, 0);
         } else {
-            var brightness = this.getBrightness();
-            if (brightness != 0) {
-                var rgb = converter.hsv.rgb([this.getHue(), this.getSaturation(), brightness]);
-                this.updateRGB(rgb[0], rgb[1], rgb[2]);
-            } else {
-                this.updateRGB(0, 0, 0);
-            }
+            var rgbw = this.hsi2rgbw(this.getHue(), this.getSaturation(), this.getBrightness());
+            this.updateRGBW(rgbw.R, rgbw.G, rgbw.B, rgbw.W);
         }
     },
 
-    updateRGB: function(red, green, blue) {
-        if ((red == green) && (red == blue)) {
-            this.log("Setting white: " + red);
-            this.rLeds.pwmWrite(0);
-            this.gLeds.pwmWrite(0);
-            this.bLeds.pwmWrite(0);
-            this.wLeds.pwmWrite(red);
+    updateRGBW: function(red, green, blue, white) {
+        this.log("Setting RGBW: " + red + ", " + green + ", " + blue + ", " + white);
+        this.rLeds.pwmWrite(red);
+        this.gLeds.pwmWrite(green);
+        this.bLeds.pwmWrite(blue);
+        this.wLeds.pwmWrite(white);
+    },
+
+    hsi2rgbw: function(H, S, I) {
+        if (H == 0 && S == 0) {
+            return {
+                R: 0,
+                G: 0,
+                B: 0,
+                W: I
+            };
         } else {
-            this.log("Setting RGB: " + red + ", " + green + ", " + blue);
-            this.rLeds.pwmWrite(red);
-            this.gLeds.pwmWrite(green);
-            this.bLeds.pwmWrite(blue);
-            this.wLeds.pwmWrite(0);
+            var segment = Math.floor(H / 60);
+            var offset = H % 60;
+            var mid = Math.round((I * offset) / 60);
+
+            var white = Math.round((I / 100) * (100 - S));
+
+            if (segment == 0) {
+                return {
+                    R: I,
+                    G: mid,
+                    B: 0,
+                    W: white
+                };
+            } else if (segment == 1) {
+                return {
+                    R: I - mid,
+                    G: I,
+                    B: 0,
+                    W: white
+                };
+            } else if (segment == 2) {
+                return {
+                    R: 0,
+                    G: I,
+                    B: mid,
+                    W: white
+                };
+            } else if (segment == 3) {
+                return {
+                    R: 0,
+                    G: I - mid,
+                    B: I,
+                    W: white
+                };
+            } else if (segment == 4) {
+                return {
+                    R: mid,
+                    G: 0,
+                    B: I,
+                    W: white
+                };
+            } else if (segment == 5) {
+                return {
+                    R: I,
+                    G: 0,
+                    B: I - mid,
+                    W: white
+                };
+            } else {
+                //Should never get here, but this will prevent a crash if it does
+                return {
+                    R: 0,
+                    G: 0,
+                    B: 0,
+                    W: 0
+                };
+            }
         }
     }
 
